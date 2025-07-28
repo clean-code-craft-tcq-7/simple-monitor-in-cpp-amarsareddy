78da5ec4c5ca1d184b64d33e7ebb9ba4fef63035
amarsareddy <66358468+amarsareddy@users.noreply.github.com>
2025-07-28 14:51:11 +0530

    File updated for complexity and modularity

--- monitor.cpp
+++ monitor.cpp
@@ -6,33 +6,60 @@
 using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
 
 int vitalsOk(float temperature, float pulseRate, float spo2) {
-  if (temperature > 102 || temperature < 95) {
-    cout << "Temperature is critical!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
-    return 0;
-  } else if (pulseRate < 60 || pulseRate > 100) {
-    cout << "Pulse Rate is out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
-    return 0;
-  } else if (spo2 < 90) {
-    cout << "Oxygen Saturation out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
-    return 0;
-  }
-  return 1;
+	return Temp_Check() && PluseRate_Check() && SpO2_Check();
+}
+
+
+int PluseRate_Check(float pulseRate)
+{
+int PulseRate_valid = 0;
+
+	if(pulseRate < 60 || pulseRate > 100)
+	{
+		PulseRate_valid = 1;
+		Call_Sleep_Print();
+	}
+
+return PulseRate_valid;	
+}
+
+int SpO2_Check( float spo2)
+{
+int SpO2_Valid = 0;
+	
+	if(spo2 < 90)
+	{
+		SpO2_Valid = 1;
+		Call_Sleep_Print();	
+	}
+	
+return SpO2_Valid;
+}
+
+int Temp_Check(float temperature)
+{	
+int Temp_valid = 0;
+	
+	if(temperature > 102 || temperature < 95)
+	{
+		Temp_valid = 1;
+		Call_Sleep_Print();
+	}
+	else{}
+	
+return Temp_valid;
+}
+
+void Call_Sleep_Print()
+{
+   int i=0;
+	
+	for (int i = 0; i < 6; i++) {
+	cout << "\r* " << flush;
+	sleep_for(seconds(1));
+	cout << "\r *" << flush;
+	sleep_for(seconds(1));
+	}
+	
+return 0;
 }
